1e0976dac98ee70a7c737fa035b6b2ff2eefd5e2
JimZhi <yf821387457@163.com>
2023-05-09 21:49:53 +0800

    帐号注册验证逻辑

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -5,6 +5,26 @@ class UserController {
     // 1.获取用户信息
     const user = ctx.request.body
 
+    // 2. 用户名密码验证
+    const { name, password } = user
+    if(!name || !password) {
+      ctx.body = {
+        code: -1001,
+        message: '用户名或者密码不能为空'
+      }
+      return
+    }
+
+    // 数据库是否存在该用户
+    const users = await userService.findUserByName(name)
+    if(users.length) {
+      ctx.body = {
+        code: -1002,
+        message: '用户名已存在'
+      }
+      return
+    }
+
     // 2.将信息存储到数据库中
     const result = await userService.create(user)
 
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -3,21 +3,21 @@ const connection = require('../app/database')
 class UserService {
   async create(user) {
     // 1.获取用户 user
-    // const { name, password } = user
+    const { name, password } = user
 
     // 2.拼接statement
-    // const statement = 'INSERT INTO `user` (name, password) VALUES (?, ?);'
+    const statement = 'INSERT INTO `users` (name, password) VALUES (?, ?);'
 
-    // 3.执行sql语句
-    // const [result] = await connection.execute(statement, [name, password])
-    // return result
+    // 3.执行sql语句 -> 异步操作
+    const [result] = await connection.execute(statement, [name, password])
+    return result
   }
 
-  // async findUserByName(name) {
-  //   const statement = 'SELECT * FROM `user` WHERE name = ?;'
-  //   const [values] = await connection.execute(statement, [name])
-  //   return values
-  // }
+  async findUserByName(name) {
+    const statement = 'SELECT * FROM `users` WHERE name = ?;'
+    const [values] = await connection.execute(statement, [name])
+    return values
+  }
 }
 
 module.exports = new UserService()
