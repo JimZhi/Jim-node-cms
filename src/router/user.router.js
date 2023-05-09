f9ea140ce61f868114b3805bd934848fb35e77ff
JimZhi <yf821387457@163.com>
2023-05-09 14:50:23 +0800

    数据库关联

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,6 +1,6 @@
 const KoaRouter = require('@koa/router')
 
-// const userController = require('../controller/user.controller')
+const userController = require('../controller/user.controller')
 // const { verifyUser, handlePassword } = require('../middleware/user.middleware')
 
 // 1.创建路由对象
@@ -8,7 +8,13 @@ const userRouter = new KoaRouter({ prefix: '/users' })
 
 // 2.定义路由中映射
 // 2.1.用户注册接口
-// userRouter.post('/', verifyUser, handlePassword , userController.create)
+// userRouter.post('/', (ctx, next) => {
+//   const user = ctx.request.body
+//   console.log(user)
+//   ctx.body = "创建成功"
+// })
+
+userRouter.post('/', userController.create)
 
 
 // 3.导出路由
